67d4771d5335f727f8e77345d92814965801065b
Alexandr Litvinov <litvinov_0104@mail.ru>
2024-02-04 18:54:07 +0300

    add reactTestingLibrary

--- .eslintrc.js
+++ .eslintrc.js
@@ -42,7 +42,16 @@ module.exports = {
         'import/no-extraneous-dependencies': 'off',
         'no-underscore-dangle': 'off',
         'i18next/no-literal-string': ['error', { markupOnly: true }],
-        'max-len': ['error', { ignoreComments: true }],
+        'max-len': [
+            'error',
+            {
+                code: 100,
+                ignoreComments: true,
+                ignoreUrls: true,
+                ignoreStrings: true,
+                ignoreTemplateLiterals: true,
+            },
+        ],
     },
     globals: {
         __IS_DEV__: true,
---
+++ src/app/providers/ErrorBoundary/ui/BugButton.tsx
@@ -0,0 +1,26 @@
+import { classNames } from 'shared/lib/classNames/classNames';
+import { Button } from 'shared/ui/Button/Button';
+import { useEffect, useState } from 'react';
+import { useTranslation } from 'react-i18next';
+
+// Компонент для тестирования
+export const BugButton = () => {
+    const [error, setError] = useState(false);
+    const { t } = useTranslation();
+    const throwError = () => setError(true);
+
+    useEffect(() => {
+        if (error) {
+            throw new Error();
+        }
+    }, [error]);
+
+    return (
+        <Button
+            onClick={throwError}
+            className={classNames('', {}, [])}
+        >
+            {t('throw error')}
+        </Button>
+    );
+};
