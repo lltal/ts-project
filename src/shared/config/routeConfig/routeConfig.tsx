e6088813edbce1192c986389e08958544f387f15
Aleksandr Litvinov <litvinov_0104@mail.ru>
2024-04-06 11:26:36 +0300

    fix tests

--- src/shared/config/routeConfig/routeConfig.tsx
+++ src/shared/config/routeConfig/routeConfig.tsx
@@ -2,6 +2,7 @@ import { RouteProps } from 'react-router-dom';
 import { MainPage } from 'pages/MainPage';
 import { AboutPage } from 'pages/AboutPage';
 import { NotFoundPage } from 'pages/NotFoundPage';
+import React from 'react';
 
 export enum AppRoutes {
     MAIN = 'main',
@@ -12,7 +13,6 @@ export enum AppRoutes {
 export const RoutePath: Record<AppRoutes, string> = {
     [AppRoutes.MAIN]: '/',
     [AppRoutes.ABOUT]: '/about',
-    // последний
     [AppRoutes.NOT_FOUND]: '*',
 };
 
