7f9073514605f06d7ea7ae9c9399d27facb44674
Aleksandr Litvinov <litvinov_0104@mail.ru>
2024-04-07 08:47:23 +0300

    begin modal

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,18 +1,27 @@
-import React, { Suspense, useEffect } from 'react';
+import React, { Suspense, useState } from 'react';
 import './styles/index.scss';
 import { classNames } from 'shared/lib/classNames/classNames';
 import { useTheme } from 'app/providers/ThemeProvider';
 import { AppRouter } from 'app/providers/router';
 import { Navbar } from 'widgets/Navbar';
 import { Sidebar } from 'widgets/Sidebar';
+import { Modal } from 'shared/ui/Modal/Modal';
 
 function App() {
     const { theme } = useTheme();
+
+    const [isOpen, setIsOpen] = useState(false);
 
     return (
         <div className={classNames('app', {}, [theme])}>
             <Suspense fallback="">
                 <Navbar />
+                <button onClick={() => setIsOpen(true)}>
+                    toggle
+                </button>
+                <Modal isOpen={isOpen} onClose={() => setIsOpen(false)}>
+                    wefopwrfopkwfropk
+                </Modal>
                 <div className="content-page">
                     <Sidebar />
                     <AppRouter />
